7b32e6b8b46725bd1d3f12fadad69955e48199e3
GWayneC <garfield.cousins@gmail.com>
2023-01-19 17:53:07 -0500

    remove calls in start function

--- node_helper.js
+++ node_helper.js
@@ -13,12 +13,12 @@ module.exports = NodeHelper.create({
     // Subclass start method.
     // Subclass format quote.
     formatquote: function(quote) {
-        //var self = this; = this;
+        //var self = this; 
        
     },
     // load quotes from json file
     getquotes: function() {
-        //var self = this; = this;
+        //var self = this; 
         console.log("getting quotes from quotes.json");
         let fileText = self.fs.readFileSync("modules/MMM-Random_Quotes/quotes.json");
         let jsonParsed = JSON.parse(fileText);
@@ -26,7 +26,7 @@ module.exports = NodeHelper.create({
         },
     // retrieve list content
     savequotes: function(quote) {
-        //var self = this; = this;
+        //var self = this;
         self.fs.writeFile("modules/MMM-Random_Quotes/quotes_updated.json" , JSON.stringify(quote), (error) => {
             if (error) throw error;
           });
@@ -52,7 +52,7 @@ module.exports = NodeHelper.create({
 	 * return quote string - A quote.
 	 */
 	randomQuote: function() {
-        //var self = this; = this;
+        //var self = this; 
 		console.log("getting random quote");
 		var quotes = self.getQuotes();
 		var index = self.randomIndex(quotes);
@@ -60,14 +60,14 @@ module.exports = NodeHelper.create({
 		return quotes[index];
 	},
     start: function() {
-        //var self = this; = this;
+        //var self = this; 
         console.log("Starting node helper for: " + this.name);
-        console.log("Starting loading quote file");
-        
+        console.log("Start loading quote file");
+        /*
         var qt = self.randomQuote();
         console.log(qt);
         self.sendNotificationTest(qt);
-        
+        */
             },
   	/* socketNotificationReceived(notification, payload)
 	 * This method is called when a socket notification arrives.
